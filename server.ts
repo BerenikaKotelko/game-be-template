97bf95c98468d2988c3f7ba5dea98f9d5f7cec9a
BeriKotelko <berenika@kotelko.com>
2022-01-26 13:27:06 +0000

    endpoints for sherlocks and victims added and functional, FLT

--- server.ts
+++ server.ts
@@ -12,10 +12,10 @@ config(); //Read .env file lines as though they were env vars.
 // false - when connecting to a local DB
 // { rejectUnauthorized: false } - when connecting to a heroku DB
 const herokuSSLSetting = { rejectUnauthorized: false };
-const sslSetting = process.env.LOCAL ? false : herokuSSLSetting;
-if (!process.env.DATABASE_URL) {
-  throw "No DATABASE_URL env var!  Have you made a .env file?  And set up dotenv?";
-}
+const sslSetting = process.env.LOCAL === "false" ? false : herokuSSLSetting;
+// if (!process.env.DATABASE_URL) {
+// throw "No DATABASE_URL env var!  Have you made a .env file?  And set up dotenv?";
+// }
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: sslSetting,
@@ -29,14 +29,47 @@ app.use(cors()); //add CORS support to each following route handler
 const client = new Client(dbConfig);
 client.connect();
 
-app.get("/", async (req, res) => {
+app.get("/victims", async (req, res) => {
   try {
-    const dbres = await client.query("select * from users");
+    const dbres = await client.query(
+      "SELECT * FROM users WHERE dead_or_alive = false"
+    );
     res.json(dbres.rows);
   } catch (err) {
-    throw err.message;
+    console.error(err.message);
   }
 });
+
+app.get("/sherlocks", async (req, res) => {
+  try {
+    const dbres = await client.query(
+      "SELECT * FROM users WHERE dead_or_alive = true"
+    );
+    res.json(dbres.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// update the studied status of a specific resource in a specific user's study list
+// app.put(
+//   "/you-died",
+//   async (req, res) => {
+//     //when do we use req.body and when to use req.params
+//     const { user_id } = req.params;
+//     const { resource_id, studied } = req.body;
+//     const dbres = await client.query(
+//       "UPDATE study_list SET studied = $1 WHERE user_id = $2 and resource_id = $3 RETURNING *",
+//       [studied, user_id, resource_id]
+//     );
+//     res.status(200).json({
+//       status: "success",
+//       message:
+//         "Updated the to_study status of a specific resource in a specific user's study list",
+//       data: dbres.rows,
+//     });
+//   }
+// );
 
 //Start the server on the given port
 const port = process.env.PORT;
